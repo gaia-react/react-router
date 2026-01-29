9e701766ba44a2e6483a90a82eafc4f9ed864107
Steven Sacks <stevensacks@gmail.com>
2026-01-29 16:55:40 +0900

    docs: fix docs deploy folder

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/docs/',
+  base: '/react-router/',
   lang: 'en-US',
   title: 'Docs',
   description:
